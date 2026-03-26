a9f2ebc2e63d79c57a4c47bd042bfb8e8770e009
GItoEdu <ito.genta.aw@teikyo-u.ac.jp>
2026-03-26 15:20:07 +0900

    修正：TextObject選択時にテキストプロパティが表示されない問題を修正 ドキュメント：グループ内オブジェクトのプロパティ表示制限を仕様として反映

--- TypeVisibilityConverter.cs
+++ TypeVisibilityConverter.cs
@@ -17,36 +17,15 @@ namespace FigCrafterApp
 
             string targetTypeName = parameter.ToString() ?? "";
 
+            Type type = value.GetType();
+
             // 直接の型一致
-            if (value.GetType().Name == targetTypeName)
+            if (type.Name == targetTypeName)
             {
                 return Visibility.Visible;
             }
 
-            // GroupObject の子に対象の型が含まれていれば表示
-            // （例: 画像ペースト時に ImageObject が GroupObject の子として存在する）
-            if (value is GroupObject group)
-            {
-                if (ContainsType(group, targetTypeName))
-                {
-                    return Visibility.Visible;
-                }
-            }
-
             return Visibility.Collapsed;
-        }
-
-        /// <summary>
-        /// GroupObject の子要素を再帰的に探索し、指定の型名が含まれるか判定
-        /// </summary>
-        private bool ContainsType(GroupObject group, string typeName)
-        {
-            foreach (var child in group.Children)
-            {
-                if (child.GetType().Name == typeName) return true;
-                if (child is GroupObject nestedGroup && ContainsType(nestedGroup, typeName)) return true;
-            }
-            return false;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
